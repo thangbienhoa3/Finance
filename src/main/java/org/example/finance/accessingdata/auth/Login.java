b212e0d10032aa6791073be0592df0e120c8f85b
vuquangthang <thangbienhoa3@gmail.com>
2025-10-19 17:34:12 +0700

    adding login + register class

--- src/main/java/org/example/finance/accessingdata/auth/Login.java
+++ src/main/java/org/example/finance/accessingdata/auth/Login.java
@@ -1,4 +1,19 @@
 package org.example.finance.accessingdata.auth;
 
+
+import org.example.finance.accessingdata.user.UserRepository;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.stereotype.Service;
+
+@Service
 public class Login {
+    private final UserRepository repo;
+    public Login(UserRepository repo) {
+        this.repo = repo;
+    }
+    public String login(String username, String password) {
+        return repo.findByUsername(username).filter(user -> user.getPassword().equals(password)).map(user -> "User logged in successfully!").orElse(
+                "Invaild username or password"
+        );
+    }
 }
--- src/main/java/org/example/finance/accessingdata/user/UserService.java
+++ src/main/java/org/example/finance/accessingdata/user/UserService.java
@@ -1,34 +1,31 @@
 package org.example.finance.accessingdata.user;
 
+import org.example.finance.accessingdata.auth.Login;
+import org.example.finance.accessingdata.auth.Register;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
     private final UserRepository repo;
+    private final Login loginService;
+    private final Register registerService;
 
-    public UserService(UserRepository repo) {
+    public UserService(UserRepository repo, Login loginService, Register registerService) {
         this.repo = repo;
+        this.loginService = loginService;
+        this.registerService = registerService;
     }
 
-
-    public String register(String username, String password, String email) {
-        if (repo.findByUsername(username).isPresent()) {
-            return "User already exists";
-        }
-        User u = new User();
-        u.setUsername(username);
-        u.setPassword(password);
-        u.setEmail(email);
-
-
-        repo.save(u);
-        return "User registered successfully!";
-    }
     public String login(String username, String password) {
-        return repo.findByUsername(username).filter(u -> u.getPassword().equals(password)).
-                map(u -> "User logged in successfully!").orElse("Invalid username or password!");
+        return loginService.login(username, password);
     }
+
+    public String register(String username, String email, String password) {
+        return registerService.register(username, email, password);
+    }
+
     public Iterable<User> getAllUser() {
         return repo.findAll();
     }
+
 }
